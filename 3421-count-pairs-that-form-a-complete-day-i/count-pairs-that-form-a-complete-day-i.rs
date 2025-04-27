4ec54f4564bc62aa3e23e905508af76d15c85205
Tyler Bailey <tylerjbailey95@gmail.com>
2025-04-27 09:50:29 -0400

    Time: 0 ms (100.00%) | Memory: 2.1 MB (75.00%) - LeetSync

---
+++ 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn count_complete_day_pairs(hours: Vec<i32>) -> i32 {
+        let mut n = hours.len();
+        let mut count = 0;
+        
+        for i in 0..n {
+            for j in i+1..n {
+                if (hours[i] + hours[j]) % 24 == 0 { count += 1; }
+            }
+        }
+
+        count
+    }
+}
