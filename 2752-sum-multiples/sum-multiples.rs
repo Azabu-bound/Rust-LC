8b3893863b297e5654eb8ca538bc8f2704f63003
Tyler Bailey <tylerjbailey95@gmail.com>
2025-05-03 15:40:01 -0400

    Time: 2 ms (8.33%) | Memory: 2.3 MB (0.00%) - LeetSync

--- 2752-sum-multiples/sum-multiples.rs
+++ 2752-sum-multiples/sum-multiples.rs
@@ -1,15 +1,12 @@
 impl Solution {
     pub fn sum_of_multiples(n: i32) -> i32 {
-        let mut nums: Vec<i32> = vec![];
+        let mut result = 0;
+        
         for num in 1..=n {
-            if num % 7 == 0 {
-                nums.push(num);
-            } else if num % 5 == 0 {
-                nums.push(num);
-            } else if num % 3 == 0 {
-                nums.push(num);
+            if num % 7 == 0 || num % 5 == 0 || num % 3 == 0 {
+                result += num;
             }
         }
-        nums.iter().sum()
+        result
     }
 }
