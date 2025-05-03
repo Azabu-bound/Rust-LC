71887b07300034705e75b96f72ccbe9a2ebaf540
Tyler Bailey <tylerjbailey95@gmail.com>
2025-05-03 10:23:43 -0400

    Time: 0 ms (100.00%) | Memory: 2.2 MB (15.79%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.rs
@@ -0,0 +1,18 @@
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn last_stone_weight(stones: Vec<i32>) -> i32 {
+        let mut heap = BinaryHeap::from(stones);
+
+        while heap.len() > 1 {
+            let x = heap.pop().unwrap();
+            let y = heap.pop().unwrap();
+
+            if x - y != 0 {
+                heap.push(x - y);
+            }
+        }
+
+        heap.pop().unwrap_or(0)
+    }
+}
