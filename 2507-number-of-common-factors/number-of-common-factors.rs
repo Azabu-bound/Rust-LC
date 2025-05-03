e2d3af539e7bb14c4fbac5ba34cd62888aae98fc
Tyler Bailey <tylerjbailey95@gmail.com>
2025-05-02 20:27:00 -0400

    Time: 0 ms (100.00%) | Memory: 2.2 MB (76.92%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.rs
+++ 2507-number-of-common-factors/number-of-common-factors.rs
@@ -1,18 +1,11 @@
 impl Solution {
     pub fn common_factors(a: i32, b: i32) -> i32 {
+        let max = a.max(b);
         let mut result = 0;
-        
-        if a >= b {
-            for x in 1..=a {
-                if a % x == 0 && b % x == 0 {
-                    result += 1;
-                }
-            }
-        } else {
-            for x in 1..=b {
-                if a % x == 0 && b % x == 0 {
-                    result += 1;
-                }
+
+        for x in 1..=max {
+            if a % x == 0 && b % x == 0 {
+                result += 1;
             }
         }
 
